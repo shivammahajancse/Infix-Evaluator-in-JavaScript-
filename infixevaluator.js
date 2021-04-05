011f3531b1023411151c2cc2bd7bda672d83197e
Shivam Mahajan <shivammahajanmarch@gmail.com>
2021-04-06 01:11:29 +0530

    while loop added

--- infixevaluator.js
+++ infixevaluator.js
@@ -143,6 +143,13 @@ function InfixtoPostfix()
                 push(el);
             }
         }
+        else{
+            postfix[temp++]=el;
+            postfix[temp++]="$"
+        }
+
         
+    }
+    
  }
  
