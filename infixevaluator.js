490354194460ebb271f6bb602217fe2b9e6b8854
Shivam Mahajan <shivammahajanmarch@gmail.com>
2021-04-06 01:00:04 +0530

    js file modified

--- infixevaluator.js
+++ infixevaluator.js
@@ -2,5 +2,38 @@ var stackarr=[];
 var topp=-1;
 var evalstack=[];
 
-
- 
+function postfixevaluator(evalst)
+{
+    eval=evalst.split("$");
+    console.log(eval);
+   for(var i=0;i<eval.length-1;i++)
+   {
+       if(!operator(eval[i]))
+       {
+           push(eval[i]);
+       }
+       else
+       {
+           var op1=parseInt(pop());
+           var op2=parseInt(pop());
+           
+           if(eval[i]=="+")
+           {
+               push(op2+op1);
+           }
+           else if(eval[i]=="-")
+           {
+               push(op2-op1);
+           }
+           else if(eval[i]=="*")
+           {
+               push(op2*op1);
+           }
+           else
+           {
+               push(op2/op1);
+           }
+       }
+   }
+   document.getElementById("text").innerHTML=(pop());
+}
